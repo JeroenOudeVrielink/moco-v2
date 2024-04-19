3657e94904d8ae3f819645381b8c24505c3df1db
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-04-19 16:32:48 +0930

    adding print

--- main_moco.py
+++ main_moco.py
@@ -523,6 +523,8 @@ def train(
 
         # compute output
         student_out, teacher_out = model(im_q=images[0], im_k=images[1])
+        print(type(student_out))
+        print(type(teacher_out))
         loss = criterion(student_out, teacher_out, epoch)
 
         # acc1/acc5 are (K+1)-way contrast classifier accuracy
